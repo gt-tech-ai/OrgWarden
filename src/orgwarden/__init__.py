6097d54bbc311180596253a1cc20728ff909a597
austin-weeks <austinweeksmusic@gmail.com>
2025-03-19 13:59:51 -0400

    refactor file structure

--- src/orgwarden/__init__.py
+++ src/orgwarden/__init__.py
@@ -1,10 +1,4 @@
-from orgwarden import repo_crawler
+from .main import main
 
-
-def main() -> None:
-    org_name = "gt-sse-center" # hard-coded for now
-    repos = repo_crawler.fetch_org_repos(org_name)
-
-    print(f"~~~~~ Public repositories found for {org_name} ~~~~~")
-    for repo in repos:
-        print(f"{repo.org}/{repo.name} - {repo.url}")
+if __name__ == "__main__":
+    main()
---
+++ src/orgwarden/main.py
@@ -0,0 +1,10 @@
+from .repo_crawler import fetch_org_repos
+
+
+def main() -> None:
+    org_name = "gt-sse-center" # hard-coded for now
+    repos = fetch_org_repos(org_name)
+
+    print(f"~~~~~ Public repositories found for {org_name} ~~~~~")
+    for repo in repos:
+        print(f"{repo.org}/{repo.name} - {repo.url}")
