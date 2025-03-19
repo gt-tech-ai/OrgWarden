a7a9dba9b24bf1b8581850c5417cebf696e3e18b
austin-weeks <austinweeksmusic@gmail.com>
2025-03-19 13:39:53 -0400

    add repo_crawler.py -> fetches repos for a given org

--- src/orgwarden/__init__.py
+++ src/orgwarden/__init__.py
@@ -1,2 +1,10 @@
+from orgwarden import repo_crawler
+
+
 def main() -> None:
-    print("Hello from orgwarden!")
+    org_name = "gt-sse-center" # hard-coded for now
+    repos = repo_crawler.fetch_org_repos(org_name)
+
+    print(f"~~~~~ Public repositories found for {org_name} ~~~~~")
+    for repo in repos:
+        print(f"{repo.org}/{repo.name} - {repo.url}")
---
+++ src/orgwarden/repo_crawler.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+import sys
+import requests
+
+
+@dataclass
+class Repository:
+    name: str
+    url: str
+    org: str
+
+def fetch_org_repos(org_name: str) -> list[Repository]:
+    """
+    Returns a `Repository` list containing the specified organization's public, non-forked repositories.
+    """
+
+    repositories: list[Repository] = []
+    page_num = 1
+    while True:
+        response = requests.get(
+            url=f"https://api.github.com/orgs/{org_name}/repos",
+            headers={
+                "Accept": "application/vnd.github+json"
+            },
+            params={
+                "per_page": 100, # max repos per page
+                "page": page_num
+            }
+        )
+        if not response.ok:
+            sys.exit(f"Error fetching repos for organization: {org_name} -> {response.json()}")
+        
+        data: list = response.json()
+        page_num += 1  # Go to the next page
+        if not data:
+            break  # No more repos
+
+        for repo in data:
+            if repo["name"] == ".github":
+                continue # ignore config repo
+            if repo["fork"]:
+                continue # ignore forks
+
+            repositories.append(Repository(
+                name=repo["name"],
+                url=repo["html_url"],
+                org=org_name
+            ))
+
+    return repositories
