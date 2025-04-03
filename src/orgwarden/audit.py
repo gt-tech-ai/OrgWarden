8e0335ad43d733cac1ad57b1df209b76a5f9fa6c
Austin Weeks <168379071+austin-weeks@users.noreply.github.com>
2025-04-03 15:46:26 -0400

    Fix: remove capture parameter from audit_repository (#10)
    - Removed `capture` parameter from `audit_repository`
    - Introduced `pytest.capfd` fixture to replace `capture` functionality

--- src/orgwarden/audit.py
+++ src/orgwarden/audit.py
@@ -2,14 +2,13 @@ import subprocess
 from orgwarden.repo_crawler import Repository
 
 
-def audit_repository(repo: Repository, capture: bool = False) -> tuple[int, str | None]:
+def audit_repository(repo: Repository) -> int:
     """
     Runs RepoAuditor against the specified repository and returns a tuple containing the exit code and the stdout if `capture` is set to True.
     """
     audit_res = subprocess.run(
         f"uv run repo_auditor --include GitHub --GitHub-url {repo.url}",
         shell=True,
-        capture_output=capture,
         text=True,
     )
-    return audit_res.returncode, audit_res.stdout if capture else None
+    return audit_res.returncode
--- tests/test_audit.py
+++ tests/test_audit.py
@@ -1,5 +1,6 @@
 from orgwarden.audit import audit_repository
 import pytest
+from pytest import CaptureFixture
 
 from orgwarden.repository import Repository
 from tests.constants import ORGWARDEN_REPO_NAME, TECH_AI_ORG_NAME
@@ -10,23 +11,24 @@ def test_no_repository():
         audit_repository()
 
 
-def test_invalid_repository():
-    exit_code, stdout = audit_repository(
+def test_invalid_repository(capfd: CaptureFixture):
+    exit_code = audit_repository(
         Repository(name="invalid-repo", url="https://example.com", org="invalid-org"),
-        capture=True,
     )
     assert exit_code != 0
+    stdout = capfd.readouterr().out
     assert "not a valid GitHub repository" in stdout
 
 
-def test_orgwarden_repo():
+def test_orgwarden_repo(capfd: CaptureFixture):
     repo = Repository(
         name=ORGWARDEN_REPO_NAME,
         url=f"https://github.com/{TECH_AI_ORG_NAME}/{ORGWARDEN_REPO_NAME}",
         org=TECH_AI_ORG_NAME,
     )
-    _, stdout = audit_repository(repo, capture=True)
+    _ = audit_repository(repo)
     # cannot check exit_code, as this is dependent on whether OrgWarden passes audit
+    stdout = capfd.readouterr().out
     assert "Results: DONE!" in stdout
     assert repo.url in stdout
     assert "not a valid GitHub repository" not in stdout
